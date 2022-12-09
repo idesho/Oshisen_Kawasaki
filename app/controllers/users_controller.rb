87ecbb4a9e0f8094dc0e8948e6be018492e5adc3
井出晶 <badminton.1110@i.softbank.jp>
2022-12-09 15:57:44 +0900

    ログイン実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,70 +1,54 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  skip_before_action :login_required, only: %i[new create]
+  before_action :set_user, only: %i[show edit update]
 
-  # GET /users or /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1 or /users/1.json
-  def show
-  end
-
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users or /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user =User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if current_user != @user
+      redirect_to parks_path
+    end
+  end
+
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1 or /users/1.json
-  def destroy
-    @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
+    @user = User.find(user_params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id),notice: "編集しました！"
+    else
+      render :edit
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.fetch(:user, {})
+  def set_user
+    @user =User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation, :image, :image_cache)
+  end
+
+  def currect_user
+    unless User.find(params[:id]).user.id.to_i == current_user.id
+      redirect_to pictures_path(current_user)
+      flash[:notice] = "権限がありません"
     end
+  end
 end
