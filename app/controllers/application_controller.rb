97d93f9b79b2acc252c3c27e3dab227b555afb64
井出晶 <badminton.1110@i.softbank.jp>
2022-12-14 15:02:51 +0900

    管理者ページの閲覧制限実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,25 @@
 class ApplicationController < ActionController::Base
-    include SessionsHelper
-    #before_action :login_required
-    before_action :store_location #userがログイン前にいた場所を覚えておく
-    def store_location
-      session[:return_to] = request.url
-      #user.save[:return_to] = request.url
-    end
-  
-    def check
-      if current_user&.admin?
-        redirect_to rails_admin_path
-      else
-        ridirect_to root_path,
-        notice: '管理者以外はアクセスできません'
-      end
-    end
-  
-    #private
-  
-    #def login_required
-    #  redirect_to new_session_path unless current_user
-    #end
+  include SessionsHelper
+  #before_action :login_required
+  before_action :store_location #userがログイン前にいた場所を覚えておく
+  before_action :admin_required, if: :admin_url?
+
+  def store_location
+    session[:return_to] = request.url
+    #user.save[:return_to] = request.url
   end
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+    #notice: "管理者以外はアクセスできません"
+  end
+
+  private
+
+  def admin_url?
+    request.fullpath.include?("/admin")
+  end
+
+  def admin_required
+    redirect_to request.url.sub(/admin.*/, ""), notice: "管理者以外はアクセス不可！" unless current_user.admin?
+  end
+end
