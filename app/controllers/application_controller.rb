f9868be39fef608f7a49ccedb547cf9af9c387c4
井出晶 <badminton.1110@i.softbank.jp>
2022-12-14 13:25:45 +0900

    管理者機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     include SessionsHelper
+    before_action :require_login
     before_action :login_required
     before_action :store_location #userがログイン前にいた場所を覚えておく
 
@@ -11,5 +12,10 @@ class ApplicationController < ActionController::Base
 
     def login_required
         redirect_to new_session_path unless current_user
+    end
+
+    def not_authenticated
+        flash[:info] = 'ログインしてください'
+        redirect_to main_app.login_path #main_appのプレフィックスをつける
     end
 end
