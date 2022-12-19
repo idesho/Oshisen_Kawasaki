3456442320b21815827329041ba4c1117fa65d57
井出晶 <badminton.1110@i.softbank.jp>
2022-12-19 21:55:56 +0900

    awsデプロイ

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,7 +49,7 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-server "54.199.11.134",
+server "13.230.167.188",
   user: "ec2-user",
   roles: %w{web app},
   ssh_options: {
