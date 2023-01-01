1ad8efbc854c74e2ebe25d49d6ab7e6d51e50708
井出晶 <badminton.1110@i.softbank.jp>
2023-01-01 21:14:03 +0900

    いらないファイルを削除

--- app/models/institution.rb
+++
@@ -1,5 +0,0 @@
-class Institution < ApplicationRecord
-    has_many :ofuros, through: :ofuro_institutions, source: :ofuro
-    has_many :ofuro_institutions
-    mount_uploader :image, ImageUploader
-end
